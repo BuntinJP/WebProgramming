5801e55adae8fde3a36519aa3c661e86f855012e
Aoki Takumi <aokitakumi72@gmail.com>
2021-12-23 09:03:19 +0900

    aoki

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { React } from "react";
 
 const containerStyle = {
-  width: "100%",
-  height: "93vh",
+    width: "100%",
+    height: "93vh",
 };
 
 const center = {
-  lat: 35.537509,
-  lng: 139.802322,
+    lat: 35.537509,
+    lng: 139.802322,
 };
 const positionJyorikuti = {
-  lat: 35.476607,
-  lng: 139.798191,
+    lat: 35.476607,
+    lng: 139.798191,
 };
 const positionYuigahama = {
-  lat: 35.310011,
-  lng: 139.544386,
+    lat: 35.310011,
+    lng: 139.544386,
 };
 
 const MyComponent = () => {
-  return (
-    <LoadScript googleMapsApiKey="AIzaSyDqSf-2YnoG-pK3fhapRfS1SpQ09v9C6rg">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-        <Marker position={positionJyorikuti} label={"呉爾羅第一上陸地"} />
-        <Marker position={positionYuigahama} label={"呉爾羅第二上陸地"} />
-      </GoogleMap>
-    </LoadScript>
-  );
+    return (
+        <LoadScript googleMapsApiKey="AIzaSyDqSf-2YnoG-pK3fhapRfS1SpQ09v9C6rg">
+            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+                <Marker position={positionJyorikuti} label={"呉爾羅第一上陸地"} />
+                <Marker position={positionYuigahama} label={"呉爾羅第二上陸地"} />
+            </GoogleMap>
+        </LoadScript>
+    );
 };
 
 /* 先生のプログラムから複数の選択ができるプルダウンみたいなやつを持ってくる。
