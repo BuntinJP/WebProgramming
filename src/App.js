e71d97d94d560344507f7887650f4dfa498262c0
Aoki Takumi <aokitakumi72@gmail.com>
2022-01-29 01:24:08 +0900

    itiouImanotokoroKanseidesu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import {
+    GoogleMap,
+    InfoWindow,
+    LoadScript,
+    Marker,
+} from "@react-google-maps/api";
 import React, { useState } from "react";
 import Sign from "./sign";
 import Header from "./Header";
 import "bulma/css/bulma.css";
 import ModalSecond from "./ModalSecond";
+import { makeStyles } from "@mui/material";
 
 const containerStyle = {
     width: "100%",
-    height: "50vh",
+    height: "70vh",
 };
 
 const center = {
-    lat: 35.537509,
-    lng: 139.802322,
+    lat: 36.18691629165214,
+    lng: 139.7157247096371,
 };
-const positionJyorikuti = {
-    lat: 35.476607,
-    lng: 139.798191,
-};
-const positionYuigahama = {
-    lat: 35.310011,
-    lng: 139.544386,
+const pTPotision = [
+    { lat: 36.17534116079143, lng: 139.72608995903704 },
+    { lat: 36.197162891006734, lng: 139.7124847311575 },
+    { lat: 36.1968831637104, lng: 139.7018258585636 },
+    { lat: 36.1945054413408, lng: 139.7053788160949 },
+    { lat: 35.66243299886032, lng: 139.63430649866487 },
+];
+const pTLabel = [
+    "ホテル7",
+    "ホテルエスハイド",
+    "ホテル山水",
+    "大和プラザ",
+    "日本大学文理学部",
+];
+const divStyle = {
+    background: "white",
+    fontSize: 8.0,
 };
 
 function Form(props) {
@@ -73,15 +89,27 @@ const App = () => {
                     <GoogleMap
                         mapContainerStyle={containerStyle}
                         center={center}
-                        zoom={10}>
-                        <Marker
-                            position={positionJyorikuti}
-                            label={"呉爾羅第一上陸地"}
-                        />
-                        <Marker
-                            position={positionYuigahama}
-                            label={"呉爾羅第二上陸地"}
-                        />
+                        zoom={14}>
+                        <InfoWindow position={pTPotision[0]}>
+                            <div style={divStyle}>
+                                <h1>{pTLabel[0]}</h1>
+                            </div>
+                        </InfoWindow>
+                        <InfoWindow position={pTPotision[1]}>
+                            <div style={divStyle}>
+                                <h1>{pTLabel[1]}</h1>
+                            </div>
+                        </InfoWindow>
+                        <InfoWindow position={pTPotision[2]}>
+                            <div style={divStyle}>
+                                <h1>{pTLabel[2]}</h1>
+                            </div>
+                        </InfoWindow>
+                        <InfoWindow position={pTPotision[3]}>
+                            <div style={divStyle}>
+                                <h1>{pTLabel[3]}</h1>
+                            </div>
+                        </InfoWindow>
                     </GoogleMap>
                 </LoadScript>
             )}
@@ -89,8 +117,14 @@ const App = () => {
                 <LoadScript googleMapsApiKey="AIzaSyDqSf-2YnoG-pK3fhapRfS1SpQ09v9C6rg">
                     <GoogleMap
                         mapContainerStyle={containerStyle}
-                        center={center}
-                        zoom={10}></GoogleMap>
+                        center={pTPotision[4]}
+                        zoom={13}>
+                        <InfoWindow position={pTPotision[4]}>
+                            <div style={divStyle}>
+                                <h1>{pTLabel[4]}</h1>
+                            </div>
+                        </InfoWindow>{" "}
+                    </GoogleMap>
                 </LoadScript>
             )}
             <Sign />
